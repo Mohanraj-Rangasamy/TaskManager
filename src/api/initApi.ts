e880b66b2f778b564d2110b8af319782a78c1fdd
Mohanraj-Rangasamy <itsmemohanr6@gmail.com>
2025-12-18 12:59:56 +0530

    API Error handling

---
+++ src/api/initApi.ts
@@ -0,0 +1,11 @@
+export async function fetchInitialData() {
+  return new Promise((resolve, reject) => {
+    const flag = false; // flag to check success or failure
+    if (flag) {
+      resolve("Initial data fetched successfully");
+      return;
+    }else{
+      reject(new Error("Initial API failed"));
+    }
+  });
+}
